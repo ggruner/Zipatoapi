e2074001cddf6e222f5e9b6d031c837db2263a61
Gregor Gruener <Gregor.Gruener@tgos.de>
2016-03-14 21:52:20 +0100

    Added get_virtual_endpoint method

--- lib/Zipatoapi.py
+++ lib/Zipatoapi.py
@@ -1,7 +1,9 @@
 # Author: Gregor Gruener
-# Version: 0.2
+# Version: 0.3
 
 # Changelog:
+# 0.3
+# - Added "get_virtual_endpoint" method
 # 0.2
 # - Added "get_virtual_endpoints_config" method
 # 0.1
@@ -157,6 +159,39 @@ class Zipatoapi:
 		c.perform()
 
 		return json.loads(output_init.getvalue())
+
+	def get_virtual_endpoint(self, uuid):
+		'''
+		Description:
+		 get virtual endpoint
+		'''
+
+		self.uuid = uuid
+
+		uri = "virtualEndpoints/"+self.uuid+"?network=false&device=false&clusterEndpoints=false&config=false&icons=true&type=false&bindings=false&descriptor=false&room=false&info=false&full=false&attributes=false"
+		api_url = self.url + uri
+		c = pycurl.Curl()
+		output_init = BytesIO()
+
+		c.setopt(c.URL, api_url)
+		### Create the cookie File
+		c.setopt(pycurl.COOKIEFILE, 'cookie.txt')
+		c.setopt(c.WRITEFUNCTION, output_init.write)
+		c.perform()
+
+		return json.loads(output_init.getvalue())
+
+	def set_value_virtual_endpoint(self, url, value):
+		'''
+		Description:
+		'''
+		self.value = value
+		self.http = url + self.value
+
+		c = pycurl.Curl()
+		c.setopt(pycurl.URL, self.http)
+		c.setopt(c.WRITEFUNCTION, output_init.write)
+		c.perform()
 
 	def create_rooms(self, data):
 		'''
